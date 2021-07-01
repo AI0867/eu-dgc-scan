7d419300f6c1c842f7e4537123ba794c6a301985
Alexander van Gessel <ai0867@gmail.com>
2021-07-01 16:08:32 +0200

    Initial functioning EU DGC QR scanner

---
+++ scan.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import base45
+import cbor
+import cose.messages
+import cv2
+import pyzbar.pyzbar
+
+cap = cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)
+
+cap.open(0)
+
+known_codes = set()
+
+while cap.isOpened():
+    success, img = cap.read()
+    if success:
+        codes = pyzbar.pyzbar.decode(img)
+        for decoded_code in codes:
+            print(decoded_code)
+            # Rest of code is mainly about where in the image
+            code = decoded_code.data
+            if code not in known_codes:
+                print(f"Found QR code: {code}")
+                b45 = base45.b45decode(code[4:])
+                print(f"Base45 decoded: {b45}")
+                cosemsg = cose.messages.Sign1Message.decode(b45[7:])
+                print(f"COSE message: {cosemsg}")
+                print(f"Payload: {cosemsg.payload}")
+                json = cbor.loads(cosemsg.payload)
+                print(f"CBOR decoded: {json}")
+                print(f"Country: {json[1]}")
+                about = json[-260][1]
+                # Check json.keys() not in [1,4,6,-260]
+                # Check json[-260].keys() not in 1
+                # Check json[-260][1].keys() not in ['ver','dob','nam','v']
+                print(f"QR code version {about['ver']})")
+                # Check json[-260][1]['nam'] not in ['gn', 'gnt', 'fn', 'fnt']
+                print(f"For {about['nam']['gn']} {about['nam']['fn']} AKA {about['nam']['gnt']} {about['nam']['fnt']} born on {about['dob']}")
+                print(f"We found {len(about['v']} vaccinations:")
+                for i, vac in enumerate(about['v']):
+                    # Check vac.keys() not in ['tg', 'vp', 'mp', 'ma', 'dn', 'sd', 'dt', 'co', 'is', 'ci']
+                    # TODO map 'ma' -> manufacturer
+                    # TODO map 'mp' -> vaccine
+                    # TODO check meaning of rest
+                    print(f"{i+1}: On {vac['dt']}, you received {vac['mp']} made by {vac['ma']} from {vac['is']} in {vac['co']}")
